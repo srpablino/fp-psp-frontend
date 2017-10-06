26040c60811eda0e6bcc96bdac82a64551c865d0
Rodrigo Villalba <rodris.jvo@gmail.com>
2017-10-06 12:35:55 -0300

    WIP. Add survey list and form to fill out a survey

--- src/app/app.js
+++ src/app/app.js
@@ -1,17 +1,15 @@
-import Bn from 'backbone'
-import Mn from 'backbone.marionette'
-import Router from './router'
-import RootView from './root/view'
+import Bn from 'backbone';
+import Mn from 'backbone.marionette';
+import Router from './router';
+import RootView from './root/view';
 
 export default Mn.Application.extend({
-    
-    region: '#main',
-    
-    onStart() {
-        const rootView = new RootView()
-        this.showView(rootView)
-        this.Router = new Router({controller: rootView})
-        Bn.history.start({pushState: true})
-    }
-    
-})
+  region: '#main',
+
+  onStart() {
+    const rootView = new RootView();
+    this.showView(rootView);
+    this.Router = new Router({ controller: rootView });
+    Bn.history.start({ pushState: true });
+  },
+});
--- src/app/root/view.js
+++ src/app/root/view.js
@@ -1,22 +1,24 @@
-import Mn from 'backbone.marionette'
-import template from './template.hbs'
-import UsersView from '../users/view'
-import OrganizationsView from '../organizations/view'
+import Mn from 'backbone.marionette';
+import template from './template.hbs';
+import UsersView from '../users/view';
+import OrganizationsView from '../organizations/view';
+import SurveysView from '../surveys/view';
 
 export default Mn.View.extend({
-    
-    template: template,
-    
-    regions: {
-        content: '#content'
-    },
-    
-    showUsers() {
-        this.getRegion('content').show(new UsersView())
-    },
-    
-    showOrganizations() {
-        this.getRegion('content').show(new OrganizationsView())
-    }
-    
-})
+  template,
+
+  regions: {
+    content: '#content'
+  },
+
+  showUsers() {
+    this.getRegion('content').show(new UsersView());
+  },
+
+  showOrganizations() {
+    this.getRegion('content').show(new OrganizationsView());
+  },
+  showSurveys() {
+    this.getRegion('content').show(new SurveysView());
+  }
+});
