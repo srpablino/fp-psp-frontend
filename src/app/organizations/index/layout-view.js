5a1deb1855a54491c0d78f6e662d27c8599f4e77
Rodrigo Villalba <rodris.jvo@gmail.com>
2017-12-11 14:56:13 -0300

    Add search spinner example for demo

--- src/app/organizations/index/layout-view.js
+++ src/app/organizations/index/layout-view.js
@@ -1,18 +1,57 @@
 import Mn from 'backbone.marionette';
 import Template from './layout-template.hbs';
 import CollectionView from './collection-view';
+import storage from '../storage';
+import utils from '../../utils';
+import { debounce, includes } from 'lodash';
 
 export default Mn.View.extend({
   template: Template,
   regions: {
     list: '#organization-list'
   },
+  events: {
+    'keyup #search': 'handleSearch'
+  },
   initialize() {
     this.collection.on('remove', this.render);
+    this.search = debounce(this.search, 300);
   },
   onRender() {
+    setTimeout(() => {
+      this.$el.find('#search').focus();
+    }, 0);
+    this.showList();
+  },
+  showList() {
     this.getRegion('list').show(
       new CollectionView({ collection: this.collection })
     );
+  },
+  handleSearch(event) {
+    let term = event.target.value;
+    let container = this.$el.find('.list-container').eq(0);
+    const section = utils.getLoadingSection(container);
+    section.loading();
+    this.getRegion('list').empty();
+    setTimeout(() => {
+      storage.findAll().then(collection => {
+        this.collection = collection;
+        this.showList();
+        section.reset();
+      });
+    }, 1000);
+  },
+  search(term) {
+    if (!term) {
+      return null;
+    }
+    let filtered = this.collection.filterByValue(term);
+
+    if (filtered && filtered.length > 0) {
+      return filtered;
+    }
+
+    return null;
   }
 });
--- src/common/utils/index.js
+++ src/common/utils/index.js
@@ -1,9 +1,11 @@
 import BackboneRadio from 'backbone.radio';
 import $ from 'jquery';
 
-const SPINNER_TPL = '<i class="fa fa-circle-o-notch fa-spin"></i>';
-const SPINNER_OVERLAY =
-  '<div class="loading-overlay"><img src="/static/images/xsmall-spinner.gif"></div>';
+const SPINNER_OVERLAY_BTN =
+  '<div class="loading-overlay-btn"><img src="/static/images/xsmall-spinner.gif"></div>';
+
+const SPINNER_OVERLAY_SECTION =
+  '<div class="loading-overlay-section"><img src="/static/images/small-spinner.gif"></div>';
 
 export const APP_MESSAGE_CHANNEL = BackboneRadio.channel('global');
 
@@ -12,18 +14,32 @@ export function getLoadingButton(elem) {
   return {
     loading: () => {
       $(elem).attr('disabled', true);
-      $(elem).before(SPINNER_OVERLAY);
+      $(elem).before(SPINNER_OVERLAY_BTN);
     },
     reset: () => {
       $(elem).removeAttr('disabled');
 
       $(elem)
-        .prev('.loading-overlay')
+        .prev('.loading-overlay-btn')
+        .remove();
+    }
+  };
+}
+
+export function getLoadingSection(elem) {
+  return {
+    loading: () => {
+      $(elem).before(SPINNER_OVERLAY_SECTION);
+    },
+    reset: () => {
+      $(elem)
+        .prev('.loading-overlay-section')
         .remove();
     }
   };
 }
 
 export default {
-  getLoadingButton
+  getLoadingButton,
+  getLoadingSection
 };
