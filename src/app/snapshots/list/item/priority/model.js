da56c356992a520be4f2606a13c9889f7e4ef36d
Mirta Gonzalez <mirti.gonz@gmail.com>
2017-12-12 17:39:08 -0300

    Add method to validate fields

--- src/app/snapshots/list/item/priority/model.js
+++ src/app/snapshots/list/item/priority/model.js
@@ -3,5 +3,23 @@ import env from '../../../../env';
 
 export default Bn.Model.extend({
   urlRoot: `${env.API}/snapshots/priority`,
-  id: 'snapshotIndicatorId'
+  id: 'snapshotIndicatorId',
+  
+  validate(attrs = {}) {
+    let errors = [];
+
+    if (attrs.reason === '') {
+      errors.push('Missing "reason" field');
+    }
+
+    if (attrs.action === '') {
+      errors.push('Missing "action" field');
+    }
+
+    if (attrs.estimated_date === '') {
+      errors.push('Missing "estimated date" field');
+    }
+
+    return errors.length > 0 ? errors : undefined;
+  }
 });
