d6f80d849c88b4d3c00784ebe38317912f548ecb
jaimeferreira11 <jaimeferreira11@gmail.com>
2017-12-27 09:38:20 -0300

    Improvements in submenu navigation
    Added css in submenu when is actived

--- src/app/organizations/show/view.js
+++ src/app/organizations/show/view.js
@@ -4,6 +4,7 @@ import FamiliesTemplate from './families/index/layout-template.hbs';
 import UsersTemplate from './users-template.hbs';
 import IndicatorsTemplate from './indicators-template.hbs';
 import FamiliesView from './families/index/layout-view';
+import $ from 'jquery';
 
 import storage from '../storage';
 
@@ -13,19 +14,37 @@ export default Mn.View.extend({
   initialize(options) {
     this.app = options.app;
     this.entity = options.entity;
+
+  },
+  events: {
+    "click #subMenuItem" : "highlight"
+  },
+  highlight(e) {
+    alert("Hola")
+    $(e.target).parent().siblings('.subActive').removeClass('subActive');
+    $(e.target).parent().addClass('subActive');
   },
 
   onRender() {
     const headerItems = storage.getSubHeaderItems(this.model);
     this.app.updateSubHeader(headerItems);
+
+    if(this.entity == null){
+      $('#sub-header .navbar-header > .navbar-brand').addClass('subActive');
+    }else{
+      $('#subMenuItem > a[href$="'+this.entity+'"]').parent().addClass('subActive');
+    }
+
   },
 
   getTemplate() {
+
     if (this.entity === 'families') {
       this.app.showViewOnRoute(new FamiliesView());
       return  Template();
     }
     if (this.entity === 'users') {
+
       return UsersTemplate;
     }
     if (this.entity === 'indicators') {
