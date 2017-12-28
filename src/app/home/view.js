cb59c8e3c0e7f808191a0128f2d2c818457f6423
bsandoval <bsandoval@sodep.com.py>
2017-12-28 16:59:33 -0300

    Counts the families by the connected user

--- src/app/home/view.js
+++ src/app/home/view.js
@@ -2,5 +2,13 @@ import Mn from 'backbone.marionette';
 import Template from './template.hbs';
 
 export default Mn.View.extend({
-  template: Template
+  template: Template,
+  initialize(options) {
+    this.totalFamilies = options.totalFamilies;
+  },
+  serializeData() {
+    return {
+      totalFamilies: this.totalFamilies
+    };
+  }
 });
