65c64e9400b47e868724bc7614fdbaea11946d05
fhermosilla <fhermosilla@hotmail.com>
2018-01-11 17:08:04 -0300

    Fix bug that no filter correctly organizations
    Signed-off-by: fhermosilla <fhermosilla@hotmail.com>

--- src/app/organizations/index/layout-view.js
+++ src/app/organizations/index/layout-view.js
@@ -43,16 +43,23 @@ export default Mn.View.extend({
     );
   },
   handleSearch() {
+    var name = this.$el.find('#search').val();
+    this.collection = new Bn.Collection(this.model.get('list'));
+    if(!name){
+      this.showList();
+      return;
+    }
     const container = this.$el.find('.list-container').eq(0);
     const section = utils.getLoadingSection(container);
     section.loading();
     this.getRegion('list').empty();
     setTimeout(() => {
-      storage.findAll().then(collection => {
-        this.collection = collection;
-        this.showList();
-        section.reset();
+      var filtered = this.collection.filter(function (organization) {
+            return organization.get("name").toLowerCase().includes(name.toLowerCase());
       });
+      this.collection = new Bn.Collection(filtered);
+      this.showList();
+      section.reset();
     }, 1000);
   },
   search(term) {
