cbf858008a1f4941d20a8dfdff94f01b3ee134c9
Carlos Benitez Flores <carlos.fpuna@gmail.com>
2018-01-19 17:58:05 -0300

    Changes in user creation, changed the order of application/organization y role selection.

--- src/app/users/add/view.js
+++ src/app/users/add/view.js
@@ -1,9 +1,9 @@
-import Backbone from 'backbone';
+import Bn from 'backbone';
 import Mn from 'backbone.marionette';
 import $ from 'jquery';
 import Template from './template.hbs';
-import Model from '../model';
-import storage from '../storage';
+import Model from './model';
+import storage from './storage';
 import utils from '../../utils';
 import FlashesService from '../../flashes/service';
 import env from '../../env';
@@ -12,7 +12,8 @@ export default Mn.View.extend({
   template: Template,
   events: {
     'click #submit': 'handleSubmit',
-    'change #select-role': 'loadOrgs'
+    'change #select-app': 'loadRoles',
+    'change #select-org': 'loadRoles'
   },
   initialize(options) {
     this.app = options.app;
@@ -24,135 +25,140 @@ export default Mn.View.extend({
       user: this.model.attributes
     };
   },
-  onRender() {
-    this.loadRoles();
+  onAttach() {
+    this.loadOrgs()
   },
-  loadSelect(list, selectId, placeholder, type, field) {
-    $(`${selectId}-placeholder`).text(placeholder);
-    $(selectId).val(placeholder);
-    $(selectId).find('option:not(:first)').remove();
+  loadOrgs() {
+    const session = this.app.getSession();
 
-    $.each(list, (index, element) => {
-      $(selectId).append(
-        $('<option></option>')
-          .attr('value', element.id)
-          .attr('data-type', type)
-          .text(element[field])
+    if (session.userHasRole('ROLE_ROOT')) {
+      //  List all applications (Hubs and Partners)
+      this.genericFetch(
+        '/applications',
+        '#select-app',
+        'Select Application',
+        'application',
+        'name'
       );
-    });
+    } else if (session.userHasRole('ROLE_HUB_ADMIN')) {
+      //  List this Hub's organizations
+      let hubId = session.get('user').application.id;
+      this.genericFetch(
+        `/applications/${hubId}/organizations`,
+        '#select-org',
+        'Select Organization',
+        'organization',
+        'name'
+      );
+    } else if (session.userHasRole('ROLE_APP_ADMIN')) {
+      //  Get logged admin user App (Organization or Partner)
+      if (session.get('user').organization !== null) {
+        let userOrganization = session.get('user').organization;
+        let list = [];
+        list.push(userOrganization);
+
+        this.loadSelect(
+          list,
+          '#select-org',
+          'Select Organization',
+          'organization',
+          'name'
+        );
+      } else if (session.get('user').application !== null) {
+        let userPartner = session.get('user').application;
+        let list = [];
+        list.push(userPartner);
+
+        this.loadSelect(
+          list,
+          '#select-app',
+          'Select Partner',
+          'application',
+          'name'
+        );
+      }
+    }
   },
-  genericFetch(path, selectId, placeholder, type, field) {
+  loadRoles() {
+    const session = this.app.getSession();
+
+    var roles = [];
+    if (session.userHasRole('ROLE_ROOT')) {
+      const appSelected = $('#select-app').find(":selected");
+
+      if (appSelected.attr('data-type') === 'hub') {
+        roles.push({role: "ROLE_HUB_ADMIN"});
+      } else if (appSelected.attr('data-type') === 'partner') {
+        roles.push({role: "ROLE_APP_ADMIN"});
+      }
+    } else if (session.userHasRole('ROLE_HUB_ADMIN')) {
+      roles.push({role: "ROLE_APP_ADMIN"});
+    } else if (session.userHasRole('ROLE_APP_ADMIN')) {
+      roles.push({role: "ROLE_USER"});
+      roles.push({role: "ROLE_SURVEY_USER"});
+    }
+
+    this.loadSelect(
+      roles,
+      '#select-role',
+      'Select Role',
+      'role',
+      'role'
+    );
+  },
+  genericFetch(path, selectId, placeholder, type, fieldDisplayed) {
     const self = this;
-    var modelCollection = new Backbone.Model();
+    var modelCollection = new Bn.Model();
     modelCollection.urlRoot = `${env.API}${path}`;
     modelCollection.fetch({
       success(response) {
         var list = response.toJSON();
-        self.loadSelect(list, selectId, placeholder, type, field);
+        self.loadSelect(list, selectId, placeholder, type, fieldDisplayed);
       }
     });
   },
-  loadRoles() {
-    this.genericFetch('/user-roles/assignableRolesByUser', '#select-role', 'Select Role', 'role', 'role');
-  },
-  loadOrgs() {
-    const roleSelected = $('#select-role').val();
+  loadSelect(list, selectId, placeholder, type, fieldDisplayed) {
+    $(`${selectId}`).show();
+    $(`${selectId}-placeholder`).text(placeholder);
+    $(selectId).val(placeholder);
+    $(selectId)
+      .find('option:not(:first)')
+      .remove();
 
-    if (roleSelected === 'ROLE_HUB_ADMIN') {
+    $.each(list, (index, element) => {
+      var dataType = type;
+      if (dataType === 'application')
+        if (element.hub)
+          dataType = 'hub';
+        else if (element.partner)
+          dataType = 'partner';
 
-      //  List All Hubs
-      this.genericFetch('/applications/hubs', '#select-org', 'Select Hub', 'application', 'name');
-
-    } else if (roleSelected === 'ROLE_APP_ADMIN') {
-
-      //  List Apps for Logged Admin User,
-      //    if ROLE_ROOT: all Partners,
-      //    if ROLE_HUB_ADMIN: this Hub's organizations
-
-      if (this.app.getSession().userHasRole('ROLE_ROOT')) {
-
-        //  List All Partners
-        this.genericFetch('/applications/partners', '#select-org', 'Select Partner', 'application', 'name');
-
-      } else if (this.app.getSession().userHasRole('ROLE_HUB_ADMIN')) {
-
-        //  List this Hub's organizations
-        let hubId = this.app.getSession().get('user').application.id;
-        this.genericFetch(`/applications/${hubId}/organizations`, '#select-org', 'Select Organization', 'organization', 'name');
-
-      }
-
-    } else if (roleSelected === 'ROLE_USER' || roleSelected === 'ROLE_SURVEY_USER') {
-
-      //  Show Logged Admin User App,
-      //    if ROLE_HUB_ADMIN: show logged admin user Hub,
-      //    if ROLE_APP_ADMIN: show logged user Partner
-
-      if (this.app.getSession().userHasRole('ROLE_HUB_ADMIN')) {
-
-        //  Get logged admin user Hub
-        let userHub = this.app.getSession().get('user').application;
-        let list = [];
-        list.push(userHub);
-
-        this.loadSelect(list, '#select-org', 'Select Hub', 'application', 'name');
-
-      } else if (this.app.getSession().userHasRole('ROLE_APP_ADMIN')) {
-
-        //  Get logged admin user App (Organization or Partner)
-
-        if (this.app.getSession().get('user').organization !== null) {
-          let userOrganization = this.app.getSession().get('user').organization;
-          let list = [];
-          list.push(userOrganization);
-
-          this.loadSelect(list, '#select-org', 'Select Organization', 'organization', 'name');
-        } else if (this.app.getSession().get('user').application !== null) {
-          let userPartner = this.app.getSession().get('user').application;
-          let list = [];
-          list.push(userPartner);
-
-          this.loadSelect(list, '#select-org', 'Select Partner', 'application', 'name');
-        }
-      }
-    }
+      $(selectId).append(
+        $('<option></option>')
+          .attr('value', element.id)
+          .attr('data-type', dataType)
+          .text(element[fieldDisplayed])
+      );
+    });
   },
   handleSubmit(event) {
     event.preventDefault();
     const button = utils.getLoadingButton(this.$el.find('#submit'));
 
-    this.user = {};
+    let userModel = new Model();
     this.$el
       .find('#form')
       .serializeArray()
       .forEach(element => {
-        this.user[element.name] = element.value;
+        userModel.set(element.name, element.value);
       });
-    this.model.set('user', this.user);
 
-    let roleSelected = $('#select-role').val();
-    this.model.set('role', roleSelected);
-
-    let optionSelected = $('#select-org').find(":selected");
-
-    if (optionSelected.attr('data-type') === "application"){
-      let application = {};
-      application.id = optionSelected.val();
-      this.model.set('application', application);
-    }
-
-    if (optionSelected.attr('data-type') === "organization"){
-      let organization = {};
-      organization.id = optionSelected.val();
-      this.model.set('organization', organization);
-    }
-
-    let errors = this.model.validate(this.model.attributes);
+    let errors = userModel.validate();
 
     if (errors) {
       errors.forEach(error => {
         FlashesService.request('add', {
-          timeout: 2000,
+          timeout: 3000,
           type: 'warning',
           title: error
         });
@@ -163,14 +169,13 @@ export default Mn.View.extend({
 
     button.loading();
 
-    this.model.urlRoot = `${env.API}/users/addUserRoleApplication`;
     storage
-      .save(this.model)
+      .save(userModel)
       .then(() => {
         button.reset();
-        Backbone.history.navigate('users', { trigger: true });
+        Bn.history.navigate('users', { trigger: true });
       })
-      .always(() => {
+      .catch(() => {
         button.reset();
       });
   }
