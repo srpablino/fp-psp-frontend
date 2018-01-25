20983b64a067b53a363c33349c5907436c2b3866
Rodrigo Villalba <rodris.jvo@gmail.com>
2018-01-25 15:12:46 -0300

    Wepback refactor done
    We delegate now all the bundling process to webpack, including the CSS and injection of resources into the HTML.

--- gulpfile.js
+++ gulpfile.js
@@ -11,79 +11,45 @@ const environment = $.util.env.type || 'development';
 const isProduction = environment === 'production';
 const webpackConfig = require('./webpack/gulp.config.js')[environment];
 const merge = require('merge-stream');
-const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const noop = require('gulp-noop');
 
 const port = $.util.env.port || 9000;
 const src = 'src/';
 const dist = 'dist/';
-const tests = 'tests/';
-
-const config = [
-  {
-    dist: 'dist/',
-    html: 'index.html'
-  },
-  {
-    dist: 'dist/login/',
-    html: 'login.html'
-  }
-];
 
 let webpackChangeHandler = function(err, stats) {
-  // $.util.log(stats.toString({
-  //     colors: $.util.colors.supportsColor,
-  //     chunks: false,
-  //     hash: false,
-  //     version: false
-  // }));
+  if (err) {
+    $.util.log('[Webpack] Error:', err);
+  }
+  $.util.log(
+    stats.toString({
+      colors: $.util.colors.supportsColor,
+      chunks: false,
+      hash: false,
+      version: false
+    })
+  );
 
-  // browserSync.reload();
-
-  // firstBuildReady = true;
-
-  // TODO implement here the reload of
-  // the browser
-  console.log('changes happened');
+  $.connect.reload();
 };
 
-gulp.task('scripts', () => {
+gulp.task('webpack', () => {
   $.webpackStream(webpackConfig, null, webpackChangeHandler)
     .on('error', function(error) {
       $.util.log($.util.colors.red(error.message));
       this.emit('end');
     })
     .pipe(gulp.dest(dist))
-    .pipe($.size({ title: 'js' }))
+    .pipe($.size({ title: 'webpack' }))
     .pipe($.connect.reload());
 });
 
-gulp.task('html', () => {
-  var tasks = config.map(resource => {
-    return gulp
-      .src(src + resource.html)
-      .pipe(rename('index.html'))
-      .pipe(gulp.dest(resource.dist))
-      .pipe($.size({ title: 'html' }))
-      .pipe($.connect.reload());
-  });
-  return merge(tasks);
-});
-
-gulp.task('html:pages', () => {
+gulp.task('pages', () => {
   return gulp
     .src(src + 'pages/*')
     .pipe(gulp.dest(dist))
-    .pipe($.size({ title: 'html:pages' }))
-    .pipe($.connect.reload());
-});
-
-gulp.task('styles', () => {
-  return gulp
-    .src(src + 'styles/main.scss')
-    .pipe($.sass({ outputStyle: isProduction ? 'compressed' : 'expanded' }))
-    .pipe(gulp.dest(dist + 'css/'))
+    .pipe($.size({ title: 'pages' }))
     .pipe($.connect.reload());
 });
 
@@ -114,25 +80,6 @@ gulp.task('static', () => {
   return merge(fonts, images);
 });
 
-gulp.task('watch', () => {
-  gulp.watch([src + 'styles/**/*.scss', src + 'common/**/*.scss'], ['styles']);
-  gulp.watch([src + 'index.html', src + 'login.html'], ['html']);
-
-  // gulp.watch(
-  //   [src + 'login_app/**/*.js', src + 'login_app/**/*.hbs'],
-  //   ['scripts:login']
-  // );
-});
-
-gulp.task('lint', () => {
-  return gulp
-    .src([src + 'app/**/*.js', tests + '**/*.js'])
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('default'));
-});
-
-gulp.task('test', $.shell.task('npm test'));
-
 gulp.task('clean', cb => {
   del([dist], cb);
 });
@@ -145,26 +92,12 @@ gulp.task('environment', () => {
   projectDir.copy(configFile, commonDir.path('env.json'), { overwrite: true });
 });
 
-gulp.task('default', ['build', 'serve', 'watch']);
+gulp.task('default', ['build', 'serve']);
 
 gulp.task('build', cb => {
   if (isProduction) {
-    $.runSequence(
-      'clean',
-      'environment',
-      'static',
-      'html:pages',
-      'scripts',
-      cb
-    );
+    $.runSequence('clean', 'environment', 'static', 'pages', 'webpack', cb);
   } else {
-    $.runSequence(
-      'clean',
-      'environment',
-      'static',
-      'html:pages',
-      'scripts',
-      cb
-    );
+    $.runSequence('clean', 'environment', 'static', 'pages', 'webpack', cb);
   }
 });
