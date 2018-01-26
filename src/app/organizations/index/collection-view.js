52d718caf484d7613be54950edad97f7057ba341
fhermosilla <fhermosilla@hotmail.com>
2018-01-26 16:29:19 -0300

    Fix bootstrap column alignments and add css corrections
    Signed-off-by: fhermosilla <fhermosilla@hotmail.com>

--- src/app/organizations/index/collection-view.js
+++ src/app/organizations/index/collection-view.js
@@ -4,16 +4,16 @@ import ItemView from './item/view';
 export default Mn.CollectionView.extend({
   childView: ItemView,
   childViewOptions: {
-    className: 'col-lg-2 col-md-3 col-sm-6 col-xs-12'
+    className: 'col-lg-2 col-md-4 col-sm-6 col-xs-12'
   },
   className: 'list-container row',
-  childEvents: {
-    'delete:model': 'handleDelete'
+  childViewEvents: {
+    'delete:model': 'onChildDeleteModel'
   },
   onRender() {
     this.$el.find('#search').focus();
   },
-  onChildviewDeleteItem(childView) {
-    this.collection.remove(childView.model);
+  // TODO: implement method to delete model
+  onChildDeleteModel() {
   }
 });
