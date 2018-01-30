f267f601423bbd440f1cb515141d9eb1f3530732
jaimeferreira11 <jaimeferreira11@gmail.com>
2018-01-30 10:44:16 -0300

    List snapshot drafts

--- src/app/snapshots_drafts/collection.js
+++ src/app/snapshots_drafts/collection.js
@@ -1,6 +1,6 @@
 import Bn from 'backbone';
-import env from '../../env';
+import env from '../env';
 
 export default Bn.Collection.extend({
-  url: `${env.API}/families`
+  url: `${env.API}/snapshots/drafts`
 });
