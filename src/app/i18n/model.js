11ba7b68cada880198262afbe0862b40626c9225
Mirta Gonzalez <mirti.gonz@gmail.com>
2018-02-19 15:23:38 -0300

    Fix bug into i18n model

--- src/app/i18n/model.js
+++ src/app/i18n/model.js
@@ -2,8 +2,11 @@ import Bn from 'backbone';
 
 export default Bn.Model.extend({
 
-    initialize: options => {
+    initialize(options) {
         this.code = options.code;
     },
-    urlRoot: () => `static/i18n/${this.code}.json`
+
+    urlRoot() {
+        return `static/i18n/${this.code}.json`;
+    }
 });
