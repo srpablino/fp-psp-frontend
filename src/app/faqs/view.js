83fda349654806f5b2eabb3660630e5e4a9e20a5
Marcos Céspedes <mcespedeso@gmail.com>
2018-03-06 12:16:23 -0300

    Internationalize FAQ, Footer, Header, Surveys

--- src/app/faqs/view.js
+++ src/app/faqs/view.js
@@ -1,6 +1,18 @@
 import Mn from 'backbone.marionette';
-import Template from './template.hbs';
+import TemplateES from './template_es.hbs';
+import TemplateEN from './template_en.hbs';
+import session from '../../common/session';
 
 export default Mn.View.extend({
-  template: Template
+  
+  template: TemplateEN,
+
+  initialize() {    
+    this.template = this.getLocale();
+  },
+
+  getLocale(){
+    return session.get('locale').indexOf('es') !== -1 ? TemplateES : TemplateEN
+  }
+
 });
