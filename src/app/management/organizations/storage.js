e7dc6f8075221ffe7d1ea90af2c1c671390295c0
Marcos Céspedes <mcespedeso@gmail.com>
2018-04-17 11:33:54 -0400

    Add organization log in subheader

--- src/app/management/organizations/storage.js
+++ src/app/management/organizations/storage.js
@@ -5,11 +5,12 @@ import Collection from './collection';
 var OrganizationsStorage = Storage.extend({
   model: Model,
   collection: Collection,
-  getSubHeaderItems(model) {
+  getSubHeaderItems(model) {    
     return {
       mainItem: {
         name: model.get('name'),
-        link: `/#organizations/${model.get('id')}`
+        link: `/#organizations/${model.get('id')}`,
+        logoUrl: model.get('logoUrl') || '/static/images/icon_logo_place.png'
       },
       navigationItems: [
         {
