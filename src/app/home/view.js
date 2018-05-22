c0a99a56a9addba6da77e04cdb42c8ccef327e8b
Rodrigo Villalba <rodris.jvo@gmail.com>
2018-05-21 21:26:26 -0400

    Bug fix. Fetch activity feed in render method

--- src/app/home/view.js
+++ src/app/home/view.js
@@ -19,16 +19,19 @@ export default Mn.View.extend({
     if (this.model) {
       this.organization = this.model.attributes;
       setTimeout(() => {
-        let logo = this.model.attributes.logoUrl || '/static/images/icon_logo_place.png';
-        $('#sub-header-image').attr('src' ,logo)
-      if(!$.isEmptyObject(this.organization.dashboard.snapshotTaken.byMonth)){
+        let logo =
+          this.model.attributes.logoUrl || '/static/images/icon_logo_place.png';
+        $('#sub-header-image').attr('src', logo);
+        if (
+          !$.isEmptyObject(this.organization.dashboard.snapshotTaken.byMonth)
+        ) {
           $('.no-data').hide();
-          this.chart();
+          this.chart(this.organization.dashboard.snapshotTaken.byMonth);
         }
       }, 0);
-      this.activities = new ActivityFeed();
-      this.activities.on('sync', this.render);
-      this.activities.fetch();
+      // this.activities = new ActivityFeed();
+      // this.activities.on('sync', this.render);
+      // this.activities.fetch();
     }
   },
 
@@ -36,91 +39,95 @@ export default Mn.View.extend({
     if (this.model.get('id')) {
       this.app.updateSubHeader(storage.getSubHeaderItems(this.model));
     }
-
-    if(this.activities){
-      const activityFeed = this.$el.find('#activity-feed-admin');
-      activityFeed.empty();
-      this.activities.each(model => {
-        const item = new FeedItem({ model });
-        activityFeed.append(item.render().el);
-      });
-    }
-
+    this.renderFeed();
   },
 
-  formatDate(key){
-    return moment(key).locale(session.get('locale')?session.get('locale'):'es').format('MMMM');
+  renderFeed() {
+    this.activities = new ActivityFeed();
+    this.activities.fetch({
+      success: () => {
+        const activityFeed = this.$el.find('#activity-feed-admin');
+        activityFeed.empty();
+        this.activities.each(model => {
+          const item = new FeedItem({ model });
+          activityFeed.append(item.render().el);
+        });
+      }
+    });
+
+    // if (this.activities) {
+    //   const activityFeed = this.$el.find('#activity-feed-admin');
+    //   activityFeed.empty();
+    //   this.activities.each(model => {
+    //     const item = new FeedItem({ model });
+    //     activityFeed.append(item.render().el);
+    //   });
+    // }
+  },
+  formatDate(key) {
+    return moment(key)
+      .locale(session.get('locale') ? session.get('locale') : 'es')
+      .format('MMMM');
   },
 
-  generateData(){
-     let snapshots = this.sortData(this.organization.dashboard.snapshotTaken.byMonth);
-     let data={};
-     data.x = [
-      'x',
-       ... _.keys(snapshots).map(key => this.formatDate(key))
-    ];
-    data.data1 = [
-        'data1',
-        ... _.values(snapshots)
-    ];
-
-
+  generateData(byMonthData) {
+    let snapshots = this.sortData(byMonthData);
+    let data = {};
+    data.x = ['x', ..._.keys(snapshots).map(key => this.formatDate(key))];
+    data.data1 = ['data1', ..._.values(snapshots)];
 
     return data;
   },
 
-  sortData(data){
+  sortData(data) {
     return Object.keys(data)
-    .sort()
-    .reduce((result, key) => {
-       result[key] = data[key];
-       return result;
-   }, {});
+      .sort()
+      .reduce((result, key) => {
+        result[key] = data[key];
+        return result;
+      }, {});
   },
 
-  chart(){
-    const data = this.generateData();
+  chart(byMonthData) {
+    const data = this.generateData(byMonthData);
     c3.generate({
-        bindto: '#bar-snapshots-taken',
-        data: {
-           x: 'x',
-          columns: [
-            data.x,
-            data.data1,
-          ],
-          names: {
-            data1: t('home.snapshots-taken'),
-          },
-          colors:{
-                data1: '#60b4ef',
-              },
-          type: 'line',
-          labels: true,
-          empty: {
-            label: {
-              text: t('home.no-data')
-            }
-          }
+      bindto: '#bar-snapshots-taken',
+      data: {
+        x: 'x',
+        columns: [data.x, data.data1],
+        names: {
+          data1: t('home.snapshots-taken')
         },
-        axis: {
-            x: {
-                type: 'category'
-            },
-            y: {
-               show: true
-             }
+        colors: {
+          data1: '#60b4ef'
         },
-        bar: {
-         width: {
-             ratio: 0.6
-            }
-         },
-        size: {
-            height: 200
-          },
-        legend: {
-            show: false
+        type: 'line',
+        labels: true,
+        empty: {
+          label: {
+            text: t('home.no-data')
           }
+        }
+      },
+      axis: {
+        x: {
+          type: 'category'
+        },
+        y: {
+          show: true
+        }
+      },
+      bar: {
+        width: {
+          ratio: 0.6
+        }
+      },
+      size: {
+        height: 200
+      },
+      legend: {
+        show: false
+      }
     });
   },
 
