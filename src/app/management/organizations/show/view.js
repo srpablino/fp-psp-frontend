042b9cc16b6be58f9ea2020b43a8f28c5ab86383
Rodrigo Villalba <rodris.jvo@gmail.com>
2018-05-30 16:50:25 -0400

    Fix error thrown by 'datatable' when render is called twice

--- src/app/management/organizations/show/view.js
+++ src/app/management/organizations/show/view.js
@@ -11,7 +11,6 @@ import usersStorage from '../../users/storage';
 import ActivityFeed from '../activities/collection';
 import FeedItem from '../activities/item/view';
 
-
 export default Mn.View.extend({
   template: Template,
   initialize(options) {
@@ -19,81 +18,111 @@ export default Mn.View.extend({
     this.entity = options.entity;
     this.organizationId = options.organizationId;
     this.organization = this.model.attributes;
-    this.activities = new ActivityFeed();
-    this.activities.on('sync', this.render);
-    this.activities.fetch();
   },
   onRender() {
     let headerItems;
-    if(this.app.getSession().userHasRole('ROLE_ROOT')){
+    if (this.app.getSession().userHasRole('ROLE_ROOT')) {
       headerItems = storage.getUserSubHeaderItems(this.model);
-    }else if(this.app.getSession().userHasRole('ROLE_HUB_ADMIN')) {
+    } else if (this.app.getSession().userHasRole('ROLE_HUB_ADMIN')) {
       headerItems = storage.getHubAdminHeaderItems(this.model);
-    }else{
+    } else {
       headerItems = storage.getSubHeaderItems(this.model);
     }
     this.app.updateSubHeader(headerItems);
 
-    if(this.app.getSession().userHasRole('ROLE_APP_ADMIN')){
+    if (this.app.getSession().userHasRole('ROLE_APP_ADMIN')) {
       $('a[href$="management/organizations"]')
         .parent()
         .addClass('subActive');
-    }else{
+    } else {
       $('#sub-header .navbar-header > .navbar-brand').addClass('subActive');
     }
 
-    const activityFeed = this.$el.find('#activity-feed-hub');
-    activityFeed.empty();
-    this.activities.each(model => {
-      const item = new FeedItem({ model });
-      activityFeed.append(item.render().el);
-    });
+    this.renderFeed();
 
     setTimeout(() => {
       this.chart();
       this.dataTables();
-
     }, 0);
-
   },
-  dataTables(){
-      $('#table-top-indicators').dataTable({
-        "lengthMenu": [[5, 10, 20, -1], [5, 10, 20, "All"]],
-        "searching": false,
-        "paging" : true,
-        "lengthChange": true,
-        "info": false,
-        "order": [[ 1, "desc" ], [ 2, "desc" ], [ 3, "desc" ]]
 
+  renderFeed() {
+    this.activities = new ActivityFeed();
+    this.activities.fetch({
+      success: () => {
+        const activityFeed = this.$el.find('#activity-feed-hub');
+        activityFeed.empty();
+        this.activities.each(model => {
+          const item = new FeedItem({ model });
+          activityFeed.append(item.render().el);
+        });
+      }
+    });
+  },
+
+  dataTables() {
+    $('#table-top-indicators').dataTable({
+      lengthMenu: [[5, 10, 20, -1], [5, 10, 20, 'All']],
+      searching: false,
+      paging: true,
+      lengthChange: true,
+      info: false,
+      order: [[1, 'desc'], [2, 'desc'], [3, 'desc']]
     });
   },
   chart() {
     c3.generate({
-        bindto: '#bar-indicators',
-        data: {
-          columns: [
-            ['Red', `${this.organization.dashboard.snapshotIndicators.count_red_indicators}`],
-            ['Yellow', `${this.organization.dashboard.snapshotIndicators.count_yellow_indicators}`],
-            ['Green', `${this.organization.dashboard.snapshotIndicators.count_green_indicators}`]
+      bindto: '#bar-indicators',
+      data: {
+        columns: [
+          [
+            'Red',
+            `${
+              this.organization.dashboard.snapshotIndicators
+                .count_red_indicators
+            }`
           ],
-          colors:{
-                Red: 'rgba(255, 0, 0, 0.8)',
-                Yellow: 'rgba(255, 255, 0, 0.7)',
-                Green: 'rgba(0, 128, 0, 0.7)'
-              },
-              names: {
-                  Red: `${t('general.red')}:  ${this.organization.dashboard.snapshotIndicators.count_red_indicators}`,
-                  Yellow: `${t('general.yellow')}: ${this.organization.dashboard.snapshotIndicators.count_yellow_indicators}`,
-                  Green: `${t('general.green')}: ${this.organization.dashboard.snapshotIndicators.count_green_indicators}`
-                  },
-          type: 'donut'
+          [
+            'Yellow',
+            `${
+              this.organization.dashboard.snapshotIndicators
+                .count_yellow_indicators
+            }`
+          ],
+          [
+            'Green',
+            `${
+              this.organization.dashboard.snapshotIndicators
+                .count_green_indicators
+            }`
+          ]
+        ],
+        colors: {
+          Red: 'rgba(255, 0, 0, 0.8)',
+          Yellow: 'rgba(255, 255, 0, 0.7)',
+          Green: 'rgba(0, 128, 0, 0.7)'
         },
-        donut: {
-          width: 50
+        names: {
+          Red: `${t('general.red')}:  ${
+            this.organization.dashboard.snapshotIndicators.count_red_indicators
+          }`,
+          Yellow: `${t('general.yellow')}: ${
+            this.organization.dashboard.snapshotIndicators
+              .count_yellow_indicators
+          }`,
+          Green: `${t('general.green')}: ${
+            this.organization.dashboard.snapshotIndicators
+              .count_green_indicators
+          }`
         },
-        size: {
-            height: 250
-          }
+        type: 'donut'
+      },
+      donut: {
+        width: 50
+      },
+      size: {
+        height: 250
+      }
     });
   },
   getTemplate() {
@@ -111,7 +140,7 @@ export default Mn.View.extend({
       let app = this.app;
       let orgModel = this.model;
       usersStorage.find().then(model => {
-        app.showViewOnRoute(new UsersView({model, app, orgModel}));
+        app.showViewOnRoute(new UsersView({ model, app, orgModel }));
       });
       return this.$el.html('');
       // return UnderConstrucionTemplate;
